46c0a908ded77a3f33d4f63b3afe497e4fa07f68
Adrian Llerena <a.llerena001@gmail.com>
2018-08-11 14:14:28 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+// Default CLI Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+// Imports part of the lesson
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
